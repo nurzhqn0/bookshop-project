a1e4181387f5a6c11ea0bfdbc60a5521cb24e5c6
nurzhqn0 <nurzhqn@gmail.com>
2024-09-09 16:32:19 +0500

    Initial Commit

---
+++ Bookshop/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bookshop
+//
+//  Created by Nurzhan Izimbetov on 09.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
